4aef6bce3dfb4920a2535f632738fed5b67bd20a
test <aryajayanivas@gmail.com>
2023-04-20 17:17:09 +0530

    v7

---
+++ script2.js
@@ -0,0 +1,95 @@
+function pg1(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i==" apply given"){
+        window.location.assign("acideg2.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
+
+function pg2(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="only the"){
+        window.location.assign("bicdie3.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
+
+function pg3(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="variable"){
+        window.location.assign("zijyou4.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
+
+function pg4(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="devide many"){
+        window.location.assign("cfbdi5.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
+
+function pg5(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="value"){
+        window.location.assign("oiplom6.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
+
+function pg6(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="elements"){
+        window.location.assign("bbauop7.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
+
+function pg7(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="algorith of many subproblem"){
+        window.location.assign("azghij8.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
+
+function pg8(){
+    let i=document.getElementById("key").value;
+    let paa;
+    if(i=="test"){
+        window.location.assign("acideg2.html")
+    }
+    else {
+        paa="Sorry! Wrong Answer"
+    }
+    document.getElementById("demo").innerHTML=paa;
+}
